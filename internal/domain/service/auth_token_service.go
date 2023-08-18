400db50f37f234910075ef40bfa69be82cdba419
tfkhdyt <tfkhdyt@proton.me>
2023-08-18 17:55:43 +0700

    feat(service/auth_token): add ParseRefreshToken to interface

--- internal/domain/service/auth_token_service.go
+++ internal/domain/service/auth_token_service.go
@@ -2,7 +2,13 @@ package service
 
 import "codeberg.org/tfkhdyt/blog-api/internal/domain/entity"
 
+type Claims struct {
+	Role   entity.Role
+	UserID int32
+}
+
 type AuthTokenService interface {
 	CreateAccessToken(id int32, role entity.Role) (string, error)
 	CreateRefreshToken(id int32, role entity.Role) (string, error)
+	ParseRefreshToken(tokenString string) (*Claims, error)
 }
